d8b213edc99325da93fcecc60b5cf4966223cb80
Sabbir Hosen <sabbircse44@gmail.com>
2022-08-08 11:04:02 +0600

    Search is working

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .code import navbar, trending_topics, recent_post
 from category.models import Category, SubCategory
+from post.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -14,4 +16,35 @@ def home(request):
         'recent_posts': recent_posts
     }
     # print(recent_posts)
-    return render(request, template_name='base/indextest.html', context=data)
+    return render(request, template_name='base/index_test.html', context=data)
+
+
+def search(request):
+    if request.method == 'GET':
+        nav_list = navbar()
+        trending_topics_list = trending_topics()
+        text = request.GET['search_text']
+        posts = Post.objects.filter(sub_category__name__icontains=text) | Post.objects.filter(title__icontains=text)
+        posts = posts.order_by('priority', 'pub_date')[0:10]
+        list_post = []
+        for post in posts:
+            title_of_post = post.title
+            temp = {
+                'id': post.id,
+                'badgeTitle': [post.sub_category.parent.name, post.sub_category.name],
+                'postTitle': post.title,
+                'postDescription': post.description,
+                'postDuration': post.read_time,
+            }
+            list_post.append(temp)
+        data = {
+            'name': 'Search for '+text,
+            'trending_topics': trending_topics_list,
+            'nav': nav_list,
+            'post_count': len(list_post),
+            'posts': list_post,
+            'search_for': text
+        }
+        return render(request, template_name='search_blog.html', context=data)
+    return redirect('/')
+
